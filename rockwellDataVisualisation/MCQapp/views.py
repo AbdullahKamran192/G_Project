79f4d117f2e8fc3187540898d40f45fec2248ec0
Abdullah <abdullahkamran192@gmail.com>
2026-04-06 21:32:20 +0100

    Worked on setting up newsdata api.

--- rockwellDataVisualisation/MCQapp/views.py
+++ rockwellDataVisualisation/MCQapp/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render, HttpResponse
+import requests
+import os
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
 
 # Create your views here.
 
+base_url = ""
 
-def home(request):
+def mcqHome(request):
+    keywords = "malware OR hackers"
+    language = "en"
+    url = f"https://newsdata.io/api/1/latest?apikey={os.getenv('NEWS_API_KEY')}&q={keywords}&language={language}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        news_data = response.json()
+        print(news_data)
+    else:
+        print(f"Failed to retreive data {response.status_code}")
+
     return render(request, "mcqPage.html")
